1e0e5ce1a9d8d3ef849984dabcce43066338c08e
RusiruMeranga <seerusiru@gmail.com>
2021-07-26 00:46:22 +0900

    text binding using data property

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Hello {{ name }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      name: "Rusiru Meranga",
+    };
+  },
+};
 </script>
 
 <style>
